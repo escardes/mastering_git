b9bda3295d1439484cfeae4988fb345a963a68d6
Eureka <eurekakabeya84@gmail.com>
2024-10-18 16:59:15 +0400

    branch dev-djo modifies index.js

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ function Sum(a, b){
     //console.log('Happy developing ✨')
     return a+b;
 }
-console.log(`Happy developing ✨ and sum is :${Sum(30, 2748)}`)
+console.log(`Happy developing ✨ and sum is :${Sum(30, 2748)}`);
+console.log(Sum(299, 398));
