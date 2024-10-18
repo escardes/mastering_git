447a0a9db7476a51271db648ff9b107b5cad2ee3
Eureka <eurekakabeya84@gmail.com>
2024-10-18 16:45:34 +0400

    modify the index.js in the dev-eureka branch

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ function Sum(a, b){
     return a+b;
 }
 console.log(`Happy developing ✨ and sum is :${Sum(30, 2748)}`)
+console.log(`Today's day is ${new Date().getDay()}/${new Date().getDate()}/${new Date().getFullYear()}`)
+console.log('This is coming from dev-eureka')
